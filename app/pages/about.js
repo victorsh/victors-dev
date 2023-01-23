0ac37bf70a850867f2e58c0c22a467d546701b7a
VictorS <victor.sh.91@gmail.com>
2023-01-22 23:50:36 -0800

    update about

--- app/pages/about.js
+++ app/pages/about.js
@@ -3,7 +3,7 @@ import { Heading, Text, Box } from '@chakra-ui/react'
 import Links from '../components/Links'
 import Header from '../components/Header'
 
-export default function About() {
+export default function About(props) {
   return (
     <div>
       <Header />
@@ -11,13 +11,16 @@ export default function About() {
       <Box maxW="2xl" m="0 auto">
         <Heading as="h1">About Me</Heading>
         <Text>
-          {"Who am I? This a question we must all face from time to time. How can we define ourselves? \
-          Is it our roles in society that determine who we are? Is it our past experiences? Where does \
-          genetics fall into this picture? \
-          It's really hard to define us as people. We are, as far as we know, the most intelligent beings \
-          in the known universe. Knowing this, how does this define us?"}
+          {"To know who I am we'll need to go back to 1991, the start of the the summer, may 30.\
+            13 days later, the Los Angeles Lakers won the NBA Championship on June 12.\
+            The internet was just getting started. Super Mario World, A Link to the Past and Sonic the Hedgehog had\
+            all released in this year. I group up in an age of rapid technological grow. Which lead to an exceptional\
+            opportunity to explore a world that's rapidly changing. In the ye old days, I can image life being very slow\
+            most of the time.\
+            \
+            \
+          "}
         </Text>
-        {"I'm so flattered you're here! "}
       </Box>
     </div>
   )
@@ -25,17 +28,17 @@ export default function About() {
 
 import prisma from '../lib/prisma'
 
-// export const getStaticProps = async () => {
-//   const feed = await prisma.post.findMany({
-//     where: { published: true },
-//     include: {
-//       author: {
-//         select: { name: true }
-//       }
-//     }
-//   })
-//   return {
-//     props: { feed },
-//     revalidate: 10
-//   }
-// }
+export const getStaticProps = async () => {
+  const feed = await prisma.post.findMany({
+    where: { published: true },
+    include: {
+      author: {
+        select: { name: true }
+      }
+    }
+  })
+  return {
+    props: { feed },
+    revalidate: 10
+  }
+}
