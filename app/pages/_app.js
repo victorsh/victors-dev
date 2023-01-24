769f6aa5eba64bca252098c8a3c348008ab8b2aa
VictorS <victor.sh.91@gmail.com>
2023-01-24 00:13:00 -0800

    added vercel analytics

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,12 +1,14 @@
 import '../styles/globals.css'
 import { ChakraProvider, theme } from '@chakra-ui/react'
 import { SessionProvider } from 'next-auth/react'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }) {
   return (
     <SessionProvider session={pageProps.session}>
       <ChakraProvider theme={theme}>
         <Component {...pageProps} />
+        <Analytics />
       </ChakraProvider>
     </SessionProvider>
   )
